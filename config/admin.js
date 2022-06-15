7d5a49753b461a2ce60635d4757925286179c287
joshuajabakumar <joshua.jabakumar@bp.com>
2022-06-15 23:46:41 +0530

    Adding Crypto for admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,31 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-});
+
+
+module.exports = ({ env }) => {
+
+  if(env('NODE_ENV') === 'production'){
+    var CryptoJS = require('crypto-js');
+    return (
+      {
+        auth: {
+          secret: env('ADMIN_JWT_SECRET'),
+        },
+        apiToken: {
+          salt: CryptoJS.MD5('swatchBack2.0', { outputLength: 16 }).toString(
+            CryptoJS.enc.Base64
+          ),
+        },
+      }
+    );
+  } else {
+    return (
+      {
+        auth: {
+          secret: env('ADMIN_JWT_SECRET'),
+        },
+        apiToken: {
+          salt: env('API_TOKEN_SALT'),
+        },
+      }
+    );
+  }
+};
