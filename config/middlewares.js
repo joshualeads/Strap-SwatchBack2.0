76ec1b33d3e180c417c90229240d0b13c7bb943e
joshuajabakumar <joshua.jabakumar@bp.com>
2022-06-15 19:55:59 +0530

    Progress till 15-June-2022 Evening - 2

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,59 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({ env }) => {{
+  if(env('NODE_ENV')==='production'){
+    return (
+      [
+        {
+          name: 'strapi::security',
+          config: {
+            contentSecurityPolicy: {
+              useDefaults: true,
+              directives: {
+                'connect-src': ["'self'", 'https:'],
+                'img-src': [
+                  "'self'",
+                  'data:',
+                  'blob:',
+                  'dl.airtable.com',
+                  env('MIDDLEWARE_AWS_S3'),
+                ],
+                'media-src': [
+                  "'self'",
+                  'data:',
+                  'blob:',
+                  'dl.airtable.com',
+                  env('MIDDLEWARE_AWS_S3'),
+                ],
+                upgradeInsecureRequests: null,
+              },
+            },
+          },
+        },
+        'strapi::errors',
+        'strapi::cors',
+        'strapi::poweredBy',
+        'strapi::logger',
+        'strapi::query',
+        'strapi::body',
+        'strapi::session',
+        'strapi::favicon',
+        'strapi::public',
+        // ... Add Additional if required
+      ]
+    );
+  } else {
+    return (
+      [
+        'strapi::errors',
+        'strapi::security',
+        'strapi::cors',
+        'strapi::poweredBy',
+        'strapi::logger',
+        'strapi::query',
+        'strapi::body',
+        'strapi::session',
+        'strapi::favicon',
+        'strapi::public',
+      ]
+    );
+  }
+}}
