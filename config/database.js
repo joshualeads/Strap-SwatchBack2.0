e754264e5f819147e41051c8a7a5735a1e475af9
joshuajabakumar <joshua.jabakumar@bp.com>
2022-06-15 19:33:17 +0530

    Progress till 15-June-2022 Evening

--- config/database.js
+++ config/database.js
@@ -1,11 +1,37 @@
 const path = require('path');
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+const parse = require('pg-connection-string').parse;
+module.exports = ({ env }) => {
+  
+  if(env('NODE_ENV') === 'production'){
+    const config = parse(process.env.DATABASE_URL)
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password
+          },
+          options: {
+            ssl: false
+          }
+        }
+      }
+    }
+  } 
+  
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  }
+};
--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,52 @@
-module.exports = ({ env }) => ({
-    email: {
-        config: {
-          provider: 'sendgrid',
-          providerOptions: {
-            apiKey: env('SENDGRID_API_KEY'),
-          },
-          settings: {
-            defaultFrom: 'joshuajabakumar@gmail.com',
-            defaultReplyTo: 'joshuajabakumar@gmail.com',
-          },
-        },
+module.exports = ({ env }) => {{
+
+    if(env('NODE_ENV') === 'production'){
+        return ({
+            upload: {
+                config: {
+                  provider: 'aws-s3',
+                  providerOptions: {
+                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                    secretAccessKey: env('AWS_ACCESS_SECRET'),
+                    region: env('AWS_REGION'),
+                    params: {
+                      Bucket: env('AWS_BUCKET'),
+                    },
+                  },
+                  actionOptions: {
+                    upload: {},
+                    uploadStream: {},
+                    delete: {},
+                  },
+                },
+            },
+            email: {
+                config: {
+                  provider: 'sendgrid',
+                  providerOptions: {
+                    apiKey: env('SENDGRID_API_KEY'),
+                  },
+                  settings: {
+                    defaultFrom: env('SENDGRID_DEFAULT_FROM'),
+                    defaultReplyTo: env('SENDGRID_DEFAULT_REPLYTO'),
+                  },
+                },
+            }
+        })
     }
-});
+
+    return {
+        email: {
+            config: {
+              provider: 'sendgrid',
+              providerOptions: {
+                apiKey: env('SENDGRID_API_KEY'),
+              },
+              settings: {
+                defaultFrom: env('SENDGRID_DEFAULT_From'),
+                defaultReplyTo: env('SENDGRID_Default_ReplyTo'),
+              },
+            },
+        }
+    }
+}};
