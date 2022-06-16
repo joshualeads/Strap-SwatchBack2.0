55751e3807d79b9a92ab1f03b587258b96db2ce4
joshuajabakumar <joshua.jabakumar@bp.com>
2022-06-16 09:42:19 +0530

    Server.js admin url fix

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,10 @@
 module.exports = ({ env }) => ({
     proxy: true,
     url: env('MY_HEROKU_URL'),
+    admin: {
+        autoOpen: false,
+        url: "/admin",
+    },
     app: { 
       keys: env.array('APP_KEYS')
     },
