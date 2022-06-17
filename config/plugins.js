dca0f2d17ea4580eb243fa91629692303cdb2412
joshuajabakumar <joshua.jabakumar@bp.com>
2022-06-17 09:37:21 +0530

    Update upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -50,6 +50,19 @@ module.exports = ({ env }) => {{
                 defaultReplyTo: env('SENDGRID_Default_ReplyTo'),
               },
             },
-        }
+        },
+        upload: {
+          config: {
+            provider: 'aws-s3',
+            providerOptions: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+              region: env('AWS_REGION'),
+              params: {
+                Bucket: env('AWS_BUCKET'),
+              },
+            }
+          },
+      },
     }
 }};
