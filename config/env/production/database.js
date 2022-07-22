32c9aaa3436b017737b399bf725d009679ac5558
joshuajabakumar <joshua.jabakumar@bp.com>
2022-07-23 04:09:24 +0530

    Digital Ocean Deployment Build

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,21 +1,25 @@
-module.exports = ({ env }) => (
-  {
-    connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST'), 
-        port: env.int('DATABASE_PORT'), 
-        database: env('DATABASE_NAME'), 
-        user: env('DATABASE_USERNAME'), 
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-        },
-      },
-      debug: false,
-    }
-  }
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
 );
+
+module.exports = ({ env }) => ({
+  connection: {  
+    client: 'postgres',
+    connection: {
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+      debug: false,
+  },
+});
 
 // Heroku Deployment
 
